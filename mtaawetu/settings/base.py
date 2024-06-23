1b4433ef3a364c0c862ae589579080a30dd342f1
John Ngugi <66136743+john-ngugi@users.noreply.github.com>
2024-06-23 18:24:05 +0300

    loading page

--- mtaawetu/settings/base.py
+++ mtaawetu/settings/base.py
@@ -57,11 +57,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOWED_ORIGINS = [
 
-    "https://mtaawetu.com"
-]
-
+CSRF_TRUSTED_ORIGINS = [ "https://mtaawetu.com"]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = 'mtaawetu.urls'
